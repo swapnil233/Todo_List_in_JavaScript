2b44fd734641d741a238b1a7498bf495bd211773
Hasan Iqbal <swapniliqbal@gmail.com>
2020-09-04 17:20:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 // Select the clear button and the date element
-const clear = document.querySelector('.clear');
-const dateElement = document.getElementById('date');
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
 
 // Select the list
-const list = document.getElementById('list');
+const list = document.getElementById("list");
 
 // Select the input
-const input = document.getElementById('input');
+const input = document.getElementById("input");
 
 function addToDo(todo) {
-    const text =
-        `<li class="item">
+  const text = `<li class="item">
             <i class="fa fa-circle-thin co" job="complete" id="0"></i>
             <p class="text">${todo}</p>
             <i class="fa fa-trash-o de" job="delete" id="0"></i> 
-        </li>`
+        </li>`;
 
-    const position = 'beforeend';
+  const position = "beforeend";
 
-    list.insertAdjacentHTML(position, text);
+  list.insertAdjacentHTML(position, text);
 }
 
 addToDo("Do dishes");
